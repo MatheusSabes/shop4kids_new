c124223efb2fab01a1626796c2561b895f473ce6
Rogerio <rogeriomateus@icloud.com>
2020-03-21 15:05:33 -0300

    changes

--- opencode/js/app.js
+++ opencode/js/app.js
@@ -1,74 +1,3 @@
- 
-if (jQuery('.related__list')) {
-    jQuery('.related__list').each(function () {
-        jQuery(this).find('.showcase__item--empty').remove();
-        jQuery(this).not('.slick-initialized').slick({
-            mobileFirst: false,
-            slidesToShow: 4,
-            slidesToScroll: 4,
-            prevArrow: `<button aria-label="prev" type="button" class="slick-prev"><</button>`,
-            nextArrow: `<button aria-label="prev" type="button" class="slick-next">></button>`,
-            responsive: [
-                {
-                    breakpoint: 426,
-                    settings: {
-                        arrows: false,
-                        dots: true,
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 767,
-                    settings: {
-                        arrows: false,
-                        dots: true,
-                        slidesToShow: 2,
-                        slidesToScroll: 2
-                    }
-                },
-                {
-                    breakpoint: 991,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3
-                    }
-                }
-            ]
-        });
-    });
-}
-
-jQuery(document).ajaxStop(function (event, xhr, settings) {
-
-
-    if (jQuery('.page-product').length > 0) {
-        jQuery('#plus').remove()
-        jQuery('#minus').remove()
-        jQuery('#quantidade').append('<button type="button" id="minus" > - </button> ');
-        jQuery('#quantidade').append('<button type="button" id="plus"   > + </button> ');
-
-    }
-});
-
-jQuery(document).on('click', '#plus', function () {
-    var qtd = jQuery('#quant');
-    var val = parseInt(qtd.val());
-
-    if (val) {
-        qtd.val(val + 1);
-    }
-})
-
-jQuery(document).on('click', '#minus', function () {
-    var qtd = jQuery('#quant');
-    var val = parseInt(qtd.val());
-
-    if ((val - 1) > 0) {
-        qtd.val(--val);
-    }
-})
-
 /* Filter Mobile */
 if (window.matchMedia("(max-width:993px)").matches) {
     var buttonFilter = document.querySelector('.button__filter'),
@@ -279,6 +208,77 @@ if (window.matchMedia("(max-width:993px)").matches) {
         });
     });
  }
+
+ 
+if (jQuery('.related__list')) {
+    jQuery('.related__list').each(function () {
+        jQuery(this).find('.showcase__item--empty').remove();
+        jQuery(this).not('.slick-initialized').slick({
+            mobileFirst: false,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            prevArrow: `<button aria-label="prev" type="button" class="slick-prev"><</button>`,
+            nextArrow: `<button aria-label="prev" type="button" class="slick-next">></button>`,
+            responsive: [
+                {
+                    breakpoint: 426,
+                    settings: {
+                        arrows: false,
+                        dots: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 767,
+                    settings: {
+                        arrows: false,
+                        dots: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 991,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3
+                    }
+                }
+            ]
+        });
+    });
+}
+
+jQuery(document).ajaxStop(function (event, xhr, settings) {
+
+
+    if (jQuery('.page-product').length > 0) {
+        jQuery('#plus').remove()
+        jQuery('#minus').remove()
+        jQuery('#quantidade').append('<button type="button" id="minus" > - </button> ');
+        jQuery('#quantidade').append('<button type="button" id="plus"   > + </button> ');
+
+    }
+});
+
+jQuery(document).on('click', '#plus', function () {
+    var qtd = jQuery('#quant');
+    var val = parseInt(qtd.val());
+
+    if (val) {
+        qtd.val(val + 1);
+    }
+})
+
+jQuery(document).on('click', '#minus', function () {
+    var qtd = jQuery('#quant');
+    var val = parseInt(qtd.val());
+
+    if ((val - 1) > 0) {
+        qtd.val(--val);
+    }
+})
 
 
 if ($('.banner--javascript .banner__list')) {
@@ -567,6 +567,41 @@ function quantityCart(quantity, button) {
     }
 }
 
+console.log('Testando')
+//Slick Variants
+jQuery( document ).ajaxComplete(function( event, xhr, settings ) {
+    if ( settings.url.indexOf("snippet=snippets/product-payment") != -1 ) {
+      console.log("Triggered ajaxComplete handler. The result is " +
+        xhr.responseText);
+    }
+})
+
+jQuery('.page-catalog .product__variant').not('.slick-initialized').slick({
+    mobileFirst: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    infinite: false,
+    prevArrow: `<button aria-label="prev" type="button" class="slick-prev"><</button>`,
+    nextArrow: `<button aria-label="prev" type="button" class="slick-next">></button>`,
+    responsive: [
+        {
+            breakpoint: 426,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }
+    ]   
+});
+if(document.querySelector('.page-catalog .product__variant')){
+}
 
 // import axios from 'axios';
 
--- src/assets/snippets/product/index.js
+++ src/assets/snippets/product/index.js
@@ -15,3 +15,39 @@ function quantityCart(quantity, button) {
         }
     }
 }
+
+console.log('Testando')
+//Slick Variants
+jQuery( document ).ajaxComplete(function( event, xhr, settings ) {
+    if ( settings.url.indexOf("snippet=snippets/product-payment") != -1 ) {
+      console.log("Triggered ajaxComplete handler. The result is " +
+        xhr.responseText);
+    }
+})
+
+jQuery('.page-catalog .product__variant').not('.slick-initialized').slick({
+    mobileFirst: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    infinite: false,
+    prevArrow: `<button aria-label="prev" type="button" class="slick-prev"><</button>`,
+    nextArrow: `<button aria-label="prev" type="button" class="slick-next">></button>`,
+    responsive: [
+        {
+            breakpoint: 426,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }
+    ]   
+});
+if(document.querySelector('.page-catalog .product__variant')){
+}
